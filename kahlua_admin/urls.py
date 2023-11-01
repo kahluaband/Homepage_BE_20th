4982f4fc5ed5fb8e567aed66460aa7c4a359c0ef
letthem <suyeon3662@naver.com>
2023-11-01 15:33:42 +0900

    #37 Feat: error correction

--- kahlua_admin/urls.py
+++ kahlua_admin/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views.tickets_views import FreshmanViewSet
+from .views.application_views import ApplicationRetrieveView
 
 app_name = 'kahlua_admin'
 
 urlpatterns = [
     path('tickets/freshman_tickets/', FreshmanViewSet.as_view({'get':'list'}), name='freshman_list'),
+    path('application/apply_forms/', ApplicationRetrieveView.as_view(), name='view_apply_forms'),
 ]
