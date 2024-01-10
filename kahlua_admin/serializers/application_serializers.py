56f71a184304952b469cf589eda50e133242b0b7
letthem <suyeon3662@naver.com>
2024-01-11 01:30:58 +0900

    #36 Feat: add filter by first preference

--- kahlua_admin/serializers/application_serializers.py
+++ kahlua_admin/serializers/application_serializers.py
@@ -6,13 +6,14 @@ class ApplyFormListSerializer(serializers.ModelSerializer):
     class Meta:
         model = ApplyForm
         ordering = ['-id']  # 최신순으로 정렬
-        fields = [ # 'name', 'phone_num', 'birthdate', 'first_preference', 'second_preference'만 가져오기
-            'name', 
-            'phone_num', 
-            'birthdate', 
-            'first_preference', 
-            'second_preference',
-        ]
+        fields = '__all__' # 모두 다 가지고 오기
+        # fields = [ # 'name', 'phone_num', 'birthdate', 'first_preference', 'second_preference'만 가져오기
+        #     'name', 
+        #     'phone_num', 
+        #     'birthdate', 
+        #     'first_preference', 
+        #     'second_preference',
+        # ]
 
 class ApplyFormDetailSerializer(serializers.ModelSerializer):
     class Meta:
